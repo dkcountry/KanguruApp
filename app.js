6a77fd0acd68b61e03fcbe6a2db5b35757e98b96
David Kang <dkang009@gmail.com>
2018-03-25 15:32:24 -0500

    adds auth

--- app.js
+++ app.js
@@ -1,23 +1,50 @@
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const Place = require("./models/places");
-const seedDB = require("./seeds");
-const Comment   = require("./models/comment");
+const express = require("express"),
+      app = express(),
+      bodyParser = require("body-parser"),
+      mongoose = require("mongoose"),
+      passport = require("passport"),
+      LocalStrategy = require("passport-local"),
+      User = require("./models/user"),
+      Place = require("./models/places"),
+      seedDB = require("./seeds"),
+      Comment   = require("./models/comment");
 
 
 mongoose.connect("mongodb://localhost/travel_blog")
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('./public'));
-
 seedDB();
+
+//passport config
+app.use(require("express-session")({
+    secret: "dk",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    next();
+});
+
+const isLoggedIn = (req, res, next) => {
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 app.get("/", function(req, res) {
     res.render("landing");
 });
 
+//INDEX - show all places
 app.get("/places", function(req, res) {
     Place.find({}, function(err, places) {
         if (err) {
@@ -58,8 +85,7 @@ app.get("/places/:id", function(req, res) {
 
 // ==========
 // Comment Routes
-
-app.get("/places/:id/comments/new", function(req, res){
+app.get("/places/:id/comments/new", isLoggedIn, function(req, res){
     Place.findById(req.params.id.replace(/\s/g,''), function(err, place){
         if(err){
             console.log(err);
@@ -69,7 +95,7 @@ app.get("/places/:id/comments/new", function(req, res){
     });
 });
 
-app.post("/places/:id/comments", function(req, res){
+app.post("/places/:id/comments", isLoggedIn, function(req, res){
     Place.findById(req.params.id.replace(/\s/g,''), function(err, place){
         if(err){
             console.log(err);
@@ -88,6 +114,41 @@ app.post("/places/:id/comments", function(req, res){
     });
 })
 
+// AUTH ROUTES
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+app.post("/register", function(req, res){
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("register")
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/places");
+        });
+    });
+});
+
+//login
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/places",
+    failureRedirect: "/login"
+}), function(req, res){
+
+});
+
+//logout
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/places");
+})
+
 app.listen("3000", "localhost", function() {
-    console.log("TravelBlog Server Running..")
+    console.log("TravelBlog Server Running..");
 });
